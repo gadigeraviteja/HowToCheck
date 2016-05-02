e97574ea7f8db79e6b7006f5c0e231a92666cf3e
gadigeraviteja <gadige.raviteja@gmail.com>
2016-05-02 10:52:24 +0530

    Update AppDelegate.m
    check

--- ImagePicker/ImagePicker/AppDelegate.m
+++ ImagePicker/ImagePicker/AppDelegate.m
@@ -78,7 +78,7 @@
     NSError *error = nil;
     NSString *failureReason = @"There was an error creating or loading the application's saved data.";
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        // Report any error we got.
+        // Report any error we got. sample
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
         dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
         dict[NSLocalizedFailureReasonErrorKey] = failureReason;
